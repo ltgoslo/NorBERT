81086050223ffd7489708246d49cfaaa20ef93c1
Andrey Kutuzov <akutuzov72@gmail.com>
2021-02-03 07:02:20 +0100

    Finalized ELMo + POS

--- benchmarking/experiments/elmo_pos.py
+++ benchmarking/experiments/elmo_pos.py
@@ -8,10 +8,11 @@ import time
 import numpy as np
 from simple_elmo import ElmoModel
 from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.utils import to_categorical
-
+import logging
+from sklearn.metrics import classification_report
 from utils.utils import read_conll
 
 
@@ -20,24 +21,25 @@ def infer_embeddings(texts, contextualized):
     elmo_vectors = contextualized.get_elmo_vectors(texts, layers="average")
     end = time.time()
     processing_time = int(end - start)
-    print(f"ELMo embeddings for your input are ready in {processing_time} seconds")
-    print(f"Tensor shape: {elmo_vectors.shape}")
+    logger.info(f"ELMo embeddings for your input are ready in {processing_time} seconds")
+    logger.info(f"Tensor shape: {elmo_vectors.shape}")
 
-    nr_words = [item for sublist in texts for item in sublist]
+    nr_words = len([item for sublist in texts for item in sublist])
 
-    feature_matrix = np.zeros(nr_words, contextualized.vector_size)
+    feature_matrix = np.zeros((nr_words, contextualized.vector_size))
     nr = 0
     for vect, sent in zip(elmo_vectors, texts):
         cropped_matrix = vect[: len(sent), :]
         for row in cropped_matrix:
             feature_matrix[nr] = row
             nr += 1
-    print(feature_matrix.shape)
-    print(feature_matrix[-1])
     return feature_matrix
 
 
 if __name__ == "__main__":
+    logging.basicConfig(format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
     arg(
@@ -63,11 +65,11 @@ if __name__ == "__main__":
     if trainfile is None or devfile is None or testfile is None:
         raise SystemExit("Not all necessary CONLL files found!")
 
-    print("Loading the datasets...")
+    logger.info("Loading the datasets...")
     train_data = read_conll(trainfile)
     dev_data = read_conll(devfile)
     test_data = read_conll(testfile)
-    print("Finished loading the datasets")
+    logger.info("Finished loading the datasets")
 
     y_train = [item for sublist in train_data[2] for item in sublist]
     y_dev = [item for sublist in dev_data[2] for item in sublist]
@@ -75,14 +77,14 @@ if __name__ == "__main__":
 
     classes = sorted(list(set(y_train)))
     num_classes = len(classes)
-    print(f"{num_classes} classes")
+    logger.info(f"{num_classes} classes")
     counter = collections.Counter(y_train)
 
-    print("===========================")
-    print("Class distribution in the training data:")
+    logger.info("===========================")
+    logger.info("Class distribution in the training data:")
     for el in counter:
-        print(el, counter[el])
-    print("===========================")
+        logger.info(f"{el}\t{counter[el]}")
+    logger.info("===========================")
 
     # Converting text labels to indexes
     y_train = [classes.index(i) for i in y_train]
@@ -91,19 +93,21 @@ if __name__ == "__main__":
 
     # Convert indexes to binary class matrix (for use with categorical_crossentropy loss)
     y_train = to_categorical(y_train, num_classes)
-    print(f"Train labels shape: {y_train.shape}")
+    logger.info(f"Train labels shape: {y_train.shape}")
     y_dev = to_categorical(y_dev, num_classes)
     y_test = to_categorical(y_test, num_classes)
 
     el_model = ElmoModel()
-    el_model.load(args.elmo, max_batch_size=32)
+    el_model.load(args.elmo, max_batch_size=64)
 
     x_sentences = train_data[1]
     x_dev_sentences = dev_data[1]
-    print(f"{len(x_sentences)} train texts")
+    x_test_sentences = test_data[1]
+    logger.info(f"{len(x_sentences)} train texts")
     average_length = np.mean([len(t) for t in x_sentences])
-    print(f"Average train text length: {average_length:.3f} words")
+    logger.info(f"Average train text length: {average_length:.3f} words")
 
+    logger.info("Inferring embeddings for train and dev...")
     x_train = infer_embeddings(x_sentences, el_model)
     x_dev = infer_embeddings(x_dev_sentences, el_model)
 
@@ -118,9 +122,9 @@ if __name__ == "__main__":
               activation="relu",
               name="Input", ))
 
-    # model.add(Dropout(0.1))  # We will use dropout after the first hidden layer
+    model.add(Dropout(0.1))  # We will use dropout after the first hidden layer
 
-    model.add(Dense(128, activation="relu"))
+    # model.add(Dense(128, activation="relu"))
 
     model.add(
         Dense(num_classes, activation="softmax", name="Output"))
@@ -128,7 +132,7 @@ if __name__ == "__main__":
         loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
     # Print out the model architecture
-    print(model.summary())
+    logger.info(model.summary())
 
     # We will monitor the dynamics of accuracy on the validation set during training
     # If it stops improving, we will stop training.
@@ -141,9 +145,30 @@ if __name__ == "__main__":
     history = model.fit(
         x_train,
         y_train,
-        epochs=10,
+        epochs=15,
         verbose=2,
         validation_data=(x_dev, y_dev),
         batch_size=64,
         callbacks=[earlystopping],
     )
+
+    logger.info("Inferring embeddings for test...")
+    x_test = infer_embeddings(x_test_sentences, el_model)
+
+    score = model.evaluate(x_test, y_test, verbose=2)
+    logger.info(f"Test loss: {score[0]:.4f}")
+    logger.info(f"Test accuracy: {score[1]:.4f}")
+
+    # We use the sklearn classification_report() function
+    # to calculate per-class F1 score on the dev set:
+    predictions = model.predict(x_test)
+    # map predictions to the binary {0, 1} range:
+    predictions = np.around(predictions)
+
+    # Convert predictions from integers back to text labels:
+    y_test_real = [classes[int(np.argmax(pred))] for pred in y_test]
+    predictions = [classes[int(np.argmax(pred))] for pred in predictions]
+
+    logger.info("Classification report for the test set:")
+    logger.info(classification_report(y_test_real, predictions))
+
