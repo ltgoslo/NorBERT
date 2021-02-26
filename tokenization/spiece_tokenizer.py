3066d7742a4eb87986bbba2f1b649f31e2250e3f
Andrey Kutuzov <akutuzov72@gmail.com>
2021-02-26 21:15:07 +0100

    Improved vocabulary building

--- tokenization/spiece_tokenizer.py
+++ tokenization/spiece_tokenizer.py
@@ -2,9 +2,12 @@
 
 import sys
 from tokenizers import SentencePieceBPETokenizer
+import gensim
 
 tokenizer = SentencePieceBPETokenizer()
 
-tokenizer.train([sys.argv[1]], vocab_size=30000)
+data = gensim.models.word2vec.LineSentence(sys.argv[1])
+
+tokenizer.train_from_iterator(data, vocab_size=30000)
 
 tokenizer.save(sys.argv[2] + ".json")
