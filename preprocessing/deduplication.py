c3abafeb15c2535b3e97b32e331ae448afe6b8b9
Andrey Kutuzov <akutuzov72@gmail.com>
2022-10-22 03:29:53 +0200

    Correct processing of multiple files

--- preprocessing/deduplication.py
+++ preprocessing/deduplication.py
@@ -1,7 +1,8 @@
 #!/bin/env python3
 # coding: utf-8
 
-# Deduplicate a plain-text file (per line)
+# Deduplicate a set of plain-text files ending with *.gz
+# Deduplication is per line
 
 import argparse
 from smart_open import open
@@ -10,6 +11,7 @@ from text_dedup.near_dedup import SimHashEmbedder
 from text_dedup.postprocess import simhash_clustering
 from text_dedup.postprocess import get_group_indices
 import os
+from os import path
 
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
@@ -26,9 +28,9 @@ if __name__ == "__main__":
     # Setting up logging:
     logging.basicConfig(
         format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO, handlers=[
-        logging.FileHandler(logfile),
-        logging.StreamHandler()
-    ]
+            logging.FileHandler(logfile),
+            logging.StreamHandler()
+        ]
     )
     logger = logging.getLogger(__name__)
 
@@ -43,32 +45,40 @@ if __name__ == "__main__":
     else:
         embeddings = set()
 
+    datafiles = []
     corpus = args.corpus
-    data = open(corpus)
+    if path.isfile(corpus):
+        datafiles.append(corpus)
+    elif path.isdir(corpus):
+        datafiles = [path.join(corpus, f) for f in os.listdir(corpus)
+                     if path.isfile(path.join(corpus, f)) and f.endswith(".gz")]
     logger.info(f"Calculating hashes of {corpus}...")
 
-    for line in data:
-        line = line.strip()
-        # We do not include very short lines in deduplication:
-        if len(line.split()) < 5:
-            short += 1
-            total += 1
-            print(line)
-            continue
-        # Blank lines are also simply printed as they are
-        if not line:
-            print()
-            continue
-        computed_hash = embedder.embed_function()(line)
-        total += 1
-        if args.mode == "near":
-            embeddings.append(computed_hash)
-        else:
-            if computed_hash in embeddings:
-                discarded += 1
+    for f in datafiles:
+        data = open(f)
+        logger.info(f"Processing {f}...")
+        for line in data:
+            line = line.strip()
+            # We do not include very short lines in deduplication:
+            if len(line.split()) < 5:
+                short += 1
+                total += 1
+                print(line)
                 continue
-            embeddings.add(computed_hash)
-            print(line)
+            # Blank lines are also simply printed as they are
+            if not line:
+                print()
+                continue
+            computed_hash = embedder.embed_function()(line)
+            total += 1
+            if args.mode == "near":
+                embeddings.append(computed_hash)
+            else:
+                if computed_hash in embeddings:
+                    discarded += 1
+                    continue
+                embeddings.add(computed_hash)
+                print(line)
     logger.info(f"Processing {total} lines complete, {len(embeddings)} unique hashes in total")
     if args.mode == "identical":
         logger.info(f"{discarded} duplicate lines discarded, {short} short lines left as is.")
