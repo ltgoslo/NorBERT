4ab98c4e095d721779a8e40cb9b06a1f81f3fef8
MariaFjodorowa <laks.salmon@gmail.com>
2026-01-20 17:07:32 +0100

    testing

--- norbert4/train_olivia.sh
+++ norbert4/train_olivia.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
 #SBATCH --job-name=norbert4hplt
 #SBATCH --account=nn10029k
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --gpus-per-node=4
 #SBATCH --tasks-per-node=4
 #SBATCH --cpus-per-task=7
 #SBATCH --partition=accel
+#SBATCH --qos=devel
 #SBATCH --mem=0
-#SBATCH --time=48:00:00
+#SBATCH --time=2:00:00
 
 echo "SLURM_TASKS_PER_NODE: $SLURM_TASKS_PER_NODE"
 echo "SLURM_JOB_NODELIST: $SLURM_JOB_NODELIST"
@@ -21,14 +22,14 @@ export WORLD_SIZE=$SLURM_NTASKS
 master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export MASTER_ADDR=$master_addr
 echo "MASTER_ADDR="$MASTER_ADDR
-
+BATCH_SIZE=1
 CMD="python3 train.py \
 --train_path \
-/cluster/work/projects/nn9851k/mariiaf/hplt/deu_Latn/tokenized_shards/ \
+/cluster/work/projects/nn9851k/mariiaf/hplt/deu_Latn/test/train \
 --tokenizer_path /cluster/work/projects/nn9851k/mariiaf/hplt/deu_Latn/tokenizer.json \
 --output_dir /cluster/work/projects/nn9851k/mariiaf/hplt/deu_Latn/norbert \
 --dataset_weights 1.0 \
---name NorBERT4_base_deu_Latn_2_nodes \
+--name NorBERT4_base_deu_Latn_1_nodes \
 --max_steps 31250 \
 --config_file configs/base.json \
 --cooldown_proportion 0.2 \
@@ -39,15 +40,16 @@ CMD="python3 train.py \
 --head_lr 0.002 \
 --warmup_proportion 0.0 \
 --z_loss_weight 0.000 \
---local_batch_size 1 \
+--local_batch_size $BATCH_SIZE \
+--max_seq_length $((8192*2*2)) \
 --optimizer muon \
 --experiment NorBERT4_base_deu_Latn \
---global_batch_size 32 \
+--global_batch_size $((256/2)) \
 --momentum 0.95 \
---hybrid_numerator 7 \
---hybrid_denominator 8 \
+--hybrid_numerator 1 \
+--hybrid_denominator 2 \
 --wd_scales"
 
 echo $CMD
 
-srun apptainer exec -B /cluster/projects/:/cluster/projects/,/cluster/work/projects/:/cluster/work/projects/ $SIF $CMD
+srun apptainer exec --nv -B /cluster/projects/:/cluster/projects/,/cluster/work/projects/:/cluster/work/projects/ $SIF $CMD
