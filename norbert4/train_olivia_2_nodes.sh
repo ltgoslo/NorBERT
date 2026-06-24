69ad9ba7505942a10ce9dcf9e99869cbde2e6978
MariaFjodorowa <laks.salmon@gmail.com>
2026-06-24 14:35:07 +0200

    triton env, upd multinode

--- norbert4/train_olivia_2_nodes.sh
+++ norbert4/train_olivia_2_nodes.sh
@@ -16,17 +16,35 @@ echo "SLURM_JOB_ID: $SLURM_JOB_ID"
 echo "SLURM_NODELIST: $SLURM_NODELIST"
 echo "SLURM_NTASKS: $SLURM_NTASKS"
 
+ml reset
+ml load NRIS/GPU
+ml load NCCL/2.26.6-GCCcore-14.2.0-CUDA-12.8.0
+
+LIBFABRIC_LIB_PATH="${EBROOTLIBFABRIC}/lib"
+LIBFABRIC_INCLUDE_PATH="${EBROOTLIBFABRIC}/include"
+NCCL_ROOT_PATH="${EBROOTNCCL}"
+AWS_OFI_NCCL_LIB_PATH="${EBROOTAWSMINOFIMINNCCL}/lib"
+CXI_LIB_PATH="/usr/lib64"
+
+export SINGULARITYENV_TRITON_LIBCUDA_PATH="/usr/local/cuda/compat/lib.real"
+export APPTAINERENV_TRITON_LIBCUDA_PATH="/usr/local/cuda/compat/lib.real"
+
 SIF="/cluster/projects/nn9851k/containers/pytorch2.7_cu2.9_py3.12_arm_nlpl.sif"
+
 LANGUAGE=${1}
 echo $LANGUAGE
 export WORLD_SIZE=$SLURM_NTASKS
-master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-export MASTER_ADDR=$master_addr
-echo "MASTER_ADDR="$MASTER_ADDR
+head_node=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+# same as in https://documentation.sigma2.no/code_development/guides/pytorch_olivia/PyTorchMultiNode.html#job-script-for-multi-node-training
+export MASTER_ADDR=$head_node
+echo "MASTER_ADDR=$MASTER_ADDR"
+export MASTER_PORT=29500
+echo "MASTER_PORT=$MASTER_PORT"
 BATCH_SIZE=1
+
+# change max_steps to 15625 for small
 CMD="python3 train.py \
---train_path \
-/cluster/work/projects/nn9851k/mariiaf/hplt/$LANGUAGE/tokenized_shards_8/train \
+--train_path /cluster/work/projects/nn9851k/mariiaf/hplt/$LANGUAGE/tokenized_shards/train \
 --tokenizer_path /cluster/work/projects/nn9851k/mariiaf/hplt/$LANGUAGE/tokenizer.json \
 --output_dir /cluster/work/projects/nn9851k/mariiaf/hplt/$LANGUAGE/norbert_2_nodes \
 --dataset_weights 1.0 \
@@ -58,4 +76,19 @@ CMD="python3 train.py \
 
 echo $CMD
 
-srun apptainer exec --nv -B /cluster/projects/:/cluster/projects/,/cluster/work/projects/:/cluster/work/projects/ $SIF $CMD
+srun apptainer exec --nv \
+  -B "${LIBFABRIC_LIB_PATH}:/opt/libfabric/lib" \
+  -B "${LIBFABRIC_INCLUDE_PATH}:/opt/libfabric/include" \
+  -B "${NCCL_ROOT_PATH}:/opt/nccl" \
+  -B "${AWS_OFI_NCCL_LIB_PATH}:/opt/aws-ofi-nccl/lib" \
+  -B "${CXI_LIB_PATH}:${CXI_LIB_PATH}" \
+  --env FI_PROVIDER="${FI_PROVIDER:-cxi}" \
+  --env FI_CXI_RX_MATCH_MODE="${FI_CXI_RX_MATCH_MODE:-hybrid}" \
+  --env NCCL_PROTO="${NCCL_PROTO:-^LL128}" \
+  --env LIBFABRIC_HOME="/opt/libfabric" \
+  --env NCCL_HOME="/opt/nccl" \
+  --env AWS_OFI_NCCL_HOME="/opt/aws-ofi-nccl" \
+  --env LD_LIBRARY_PATH="${LIBFABRIC_HOME}/lib:${NCCL_HOME}/lib:${AWS_OFI_NCCL_HOME}/lib:/usr/lib64:${LD_LIBRARY_PATH}" \
+  --env CPATH="${LIBFABRIC_HOME}/include:${CPATH:-}" \
+  -B /cluster/projects/:/cluster/projects/,/cluster/work/projects/:/cluster/work/projects/ $SIF \
+  $CMD
